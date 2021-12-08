29aa8284d23bccbccee1629373787735fbc71ef0
Nechols87 <92054506+Nechols87@users.noreply.github.com>
2021-12-07 22:08:45 -0600

    Added Bcrypt

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,9 +1,11 @@
 import { Client } from "https://deno.land/x/postgres/mod.ts"
+import * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
 import { dbCreds } from '../config.ts'
 
 const client = new Client(dbCreds);
 
 const addUser = async ({ request, response }: {request: any, response: any }) => {
+   
     const body = await request.body();
     // console.log(body);
     const user = await body.value
@@ -17,10 +19,12 @@ const addUser = async ({ request, response }: {request: any, response: any }) =>
     } else {
         try {
         await client.connect()
-         
+        const salt = await bcrypt.genSalt(8);
+        const hash = await bcrypt.hash(user.password, salt);
+        console.log(`hash: ${hash}`); 
         const result = await client.queryArray("INSERT INTO registration(username, password, email, firstname, lastname) VALUES($1,$2,$3,$4,$5) RETURNING id", 
         user.username, 
-        user.password,
+        hash,
         user.email,
         user.firstname,
         user.lastname)
@@ -43,6 +47,7 @@ const addUser = async ({ request, response }: {request: any, response: any }) =>
   }  
 }
 
+
 const loginUser = async ({ request, response }: {request: any, response: any }) => {
     const body = await request.body();
     const user = await body.value
@@ -56,14 +61,15 @@ const loginUser = async ({ request, response }: {request: any, response: any }) 
     } else {
         try {
         await client.connect()
-         
+
         const result: any = await client.queryObject(`SELECT * FROM registration WHERE username = '${username}'`)
-        console.log(result.rows[0])    
-        if (result.rows[0].password === password){
+        const isValid = await bcrypt.compare(password, result.rows[0].password);
+        console.log(`isValid: ${isValid}`);    
+        if (isValid === true){
           response.status = 201
           response.body = {
             success: true,
-            data: user
+            data: "You have entered a correct password"
           }
         }
         else {
@@ -80,8 +86,9 @@ const loginUser = async ({ request, response }: {request: any, response: any }) 
         }
     } finally {
         await client.end()
-    }  
-  } 
+    } 
+  }  
 }
+
 
 export { addUser, loginUser }
--- server.ts
+++ server.ts
@@ -1,11 +1,13 @@
 import { Application } from "https://deno.land/x/oak/mod.ts"
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
-import router from './routes.ts'
 import { Session } from "https://deno.land/x/session/mod.ts";
+import router from './routes.ts'
 
 
 const port =  Deno.env.get("PORT") || 3000
 const app = new Application()
+
+
 
 // Configuring Session for the Oak framework
 const session = new Session({ 
